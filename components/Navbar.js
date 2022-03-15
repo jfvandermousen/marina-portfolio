6d1df0ff44e087035404eeb577c629eaf620a5da
jfvandermousen <jeanfrancoisvandermousen@gmail.com>
2022-03-15 13:02:01 +0100

    create pages/routes about, contact,shop

---
+++ components/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav>
+        <div className='logo'>
+            <h1>Marina Collages</h1>
+        </div>
+        <a>Home</a>
+        <a>About</a>
+        <a>Contact</a>
+        <a>Shop</a>
+    </nav>
+  )
+}
+
+export default Navbar
---
+++ pages/about.js
@@ -0,0 +1,10 @@
+const about = () => {
+  return (
+    <div>
+        <h1>About</h1>
+        <p>Do occaecat laboris incididunt cillum enim ipsum quis laboris ipsum ipsum. Magna incididunt amet et ullamco Lorem cupidatat cupidatat pariatur ex commodo consectetur officia mollit ut. Cillum consectetur laboris laborum ad ea veniam dolore laboris Lorem enim. Nulla culpa et ipsum tempor excepteur cillum deserunt adipisicing qui duis aute mollit reprehenderit labore. Deserunt qui qui id laboris. Officia nostrud ea sit excepteur minim. Anim Lorem dolore nisi veniam aute laboris.</p>
+    </div>
+  )
+}
+
+export default about;
