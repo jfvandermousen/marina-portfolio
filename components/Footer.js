3cd71cf06a85547c76e62e459ac536614b57554c
jfvandermousen <jeanfrancoisvandermousen@gmail.com>
2022-03-17 12:37:43 +0100

    create custom error page, redirection useEffect, useRouter, add some global style

--- components/Footer.js
+++ components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>&copy; 2022 - Marina Collages</div>
+    <footer>Copyright 2022 - Marina Collages</footer>
   )
 }
 
--- pages/404.js
+++ pages/404.js
@@ -1,8 +1,24 @@
+import Link from "next/link"
+import { useEffect } from "react"
+import { useRouter } from "next/router"
+
 
 const ErrorPage = () => {
+  const router = useRouter();
+
+// redirect to Homepage after 3sec
+
+
+  useEffect(()=>{
+    setTimeout(()=>{
+      router.push('/')
+    },3000)
+  },[router])
+
   return (
-    <div>
+    <div className="error-page">
         <h2>Oups there is nothing here !</h2>
+        <p>Go back to the <Link href="/">Homepage</Link></p>
     </div>
   )
 }
